e082e1a4f97ef0d25b08b5d29d4eb6b89d42142d
François Cordier <francoiscordier.ece@gmail.com>
2022-01-14 13:22:04 +0100

    expo publish

--- api/myWallet.js
+++ api/myWallet.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const myWalletAPI = axios.create({
-  baseURL: "http://192.168.1.65:3000", // Adresse IP du PC qui host le backend
+  baseURL: "https://mywallet-back-prod.herokuapp.com", // Adresse IP du PC qui host le backend
   timeout: 1000,
 });
 
